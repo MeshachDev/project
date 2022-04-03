0d8730ef7383568b64d1a5c66a4293fb195f9d17
MeshachDeveloperTerminalByte <62976486+MeshachDeveloperTerminalByte@users.noreply.github.com>
2022-04-03 17:40:29 +0530

    Ui

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-const body = document.querySelector("body")
+const body = document.querySelector("body");
 const header = document.querySelector("header");
-const root = document.querySelector(':root');
-window.addEventListener('scroll',()=>{
-    header.classList.toggle("sticky", window.scrollY > 0);
-})
+const main = document.querySelector(".main-container");
+const root = document.querySelector(":root");
+window.addEventListener("scroll", () => {
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+const ulogo = document.querySelector("#ulogo");
+const uname = document.querySelector("#uname");
+const side = document.querySelector(".side");
+const ham = document.querySelector("#ham");
+const error = document.querySelector("#error");
+ham.addEventListener("click", () => {
+  sh();
+});
+
+const sh = () => {
+  side.classList.toggle("sideon");
+};
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  const name = document.querySelector("#name").value;
+  alert("Logged In!");
+});
